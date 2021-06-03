44b6a6f09b7149ed24ff9a38952bebfd90a5bd7a
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-06-03 16:02:43 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const Product_Number={
+    Mask:999,
+    Sanitizer:500,
+    paracetamol:900
+};
+
+const Product_Price={
+    mask:20,
+    Sanitizer:100,
+    paracetamol:50
+}
+ const Total_Price=Product_Price.mask+Product_Price.Sanitizer+Product_Price.paracetamol;
+ console.log(Total_Price)
